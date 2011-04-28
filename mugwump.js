b071d23df5586782c3b012d422d1c277fa5891d2
Charles Wang <cwang@realpractice.com>
2011-04-27 17:28:52 -0700

    functionality and design of mugwump game completed

--- mugwump.js
+++ mugwump.js
@@ -1,11 +1,9 @@
 /*jslint white: true, onevar: true, undef: true, nomen: true, regexp: true, plusplus: true, bitwise: true, newcap: true, maxerr: 50, indent: 4 */
-var input = function () {
-    var value;
-    do {
-        value = prompt("Input a value: ");
-    } while (value === null || value === "");
-    return value;
-};
+var counter = 0,
+    found = [false, false, false, false],
+    mugs_found = 0,
+    results_msg = "",
+    mugs = [];
 
 function Mugwamp() {
     this.pos_x = Math.floor(Math.random() * 10);
@@ -28,14 +26,35 @@ function calculate_difference(mug, x, y) {
     return parseInt(difference, 10);
 }
 
-function find_mugwump(mugs, found, x, y) {
+function clear_board(opts) {
+  if (typeof opts == "undefined") {
+      opts = false;
+    }
+  if (opts) {
+    var elems = $('.found');
+    elems.each(function(index,element){
+      $(element).removeClass('found');
+    });
+  } 
+  var el = $('.miss');
+  el.each(function(index,element){
+    $(element).removeClass('miss');
+  });
+}
+
+function find_mugwump(x, y) {
+    clear_board();
+    var x = x.val();
+    var y = y.val();
+    var found_flag = false;
     var diff = [0, 0, 0, 0],
         i = 0;
     for (; i < 4; i += 1) {
         if (found[i] === false) {
-            if (mugs[i].pos_x === x) {
-                if (mugs[i].pos_y === y) {
+            if (mugs[i].pos_x + "" === x) {
+                if (mugs[i].pos_y + "" === y) {
                     found[i] = true;
+                    found_flag = true;
                 } else {
                     diff[i] = calculate_difference(mugs[i], x, y);
                 }
@@ -44,63 +63,87 @@ function find_mugwump(mugs, found, x, y) {
             }
         }
     }
+    draw(x,y,found_flag);
     return {
         found_result: found,
         diff_result: diff
     };
 }
 
-var game = function (mugs) {
-        var found = [false, false, false, false],
-            count = 0,
-            x = "",
-            y = "",
-            mugs_found = 0,
-            results = "",
-            results_msg = "",
-            i = 0,
-            fail_msg = "";
-        while (count < 10) {
-            count += 1;
-            x = input();
-            y = input();
-            results = find_mugwump(mugs, found, x, y);
-            for (; i < 4; i += 1) {
-                if (results.found_result[i] === true) {
-                    mugs_found += 1;
-                    results_msg += "YOU HAVE FOUND MUGWUMP " + (i + 1) + "\n";
-                } else {
-                    results_msg += "You are " + results.diff_result[i] + " units from mugwamp " + (i + 1) + "\n";
-                }
-            }
-            alert(results_msg);
+function draw(x, y, found) {
+  if (found) {
+    $($('#row' + y + " td")[x]).addClass('found');
+  } else {
+    $($('#row' + y + " td")[x]).addClass('miss');
+  }
+}
 
-            // Winner conditions
-            if (mugs_found === 4) {
-                alert("You found all the mugwumps");
-                alert("You got them all in " + count + " turns!");
-                break;
-            }
+function input() {
+  var x = $('#xval');
+  var y = $('#yval');
+  if (counter === 10) {
+    alert('Game Over! Click start to play a new game');
+  } else {
+    results = find_mugwump(x,y);
+    display_results(results);
+    counter += 1;
+    check_winner();
+  }
+  
+}
 
-            // Loser conditions
-            if (count === 10) {
-                fail_msg = "SORRY, THAT'S 10 TRIES.  HERE IS WHERE THEY'RE HIDING:\n\n";
-                for (; i < 4; i += 1) {
-                    fail_msg += "MUGWUMP " + (i + 1) + " is at (" + mugs.pos_x + "," + mugs.pos_y + ")!";
-                }
-            }
-        }
-        alert("THAT WAS FUN! LET'S PLAY AGAIN.......\nFOUR MORE MUGWUMPS ARE NOW IN HIDING.");
-    };
+function check_winner() {
+  // Winner conditions
+  if (mugs_found === 4) {
+      alert("You found all the mugwumps");
+      results_msg = "You got them all in " + counter + " turns!</br>"  + "THAT WAS FUN! LET'S PLAY AGAIN.......<br />FOUR MORE MUGWUMPS ARE NOW IN HIDING."
+      print_message(results_msg);
+      $('#xval, #yval').attr('disabled', false);
+  }
+
+  // Loser conditions
+  if (counter === 10 && mugs_found !== 4) {
+      results_msg = "SORRY, THAT'S 10 TRIES.  HERE IS WHERE THEY'RE HIDING:<br /><br />";
+      for (; i < 4; i += 1) {
+          results_msg += "MUGWUMP " + (i + 1) + " is at (" + mugs.pos_x + "," + mugs.pos_y + ")!<br />THAT WAS FUN! LET'S PLAY AGAIN.......<br />FOUR MORE MUGWUMPS ARE NOW IN HIDING.";
+      }
+      print_message(results_msg);
+      $('#xval, #yval').attr('disabled', false);    
+  }
+}
+
+function reset_game() {
+      counter = 0;
+      found = [false, false, false, false];
+      mugs_found = 0;
+      results_msg = "";
+      mugs = [];
+      clear_board(true);
+      var new_msg = "Game has started.<br /> Please input two values between 0-9."
+      print_message(new_msg);
+      $('#xval, #yval').val('');
+      $('#xval, #yval').attr('disabled', false);
+}
+
+function print_message(message) {
+  $('#message_box').empty();
+  $('#message_box').html(message);
+}
+
+function display_results() {
+  var i = 0;
+  for (; i < 4; i += 1) {
+      if (results.found_result[i] === true) {
+          mugs_found += 1;
+          results_msg += "YOU HAVE FOUND MUGWUMP " + (i + 1) + "<br />";
+      } else {
+          results_msg += "You are " + results.diff_result[i] + " units from mugwamp " + (i + 1) + "<br />";
+      }
+  }
+  print_message(results_msg);
+}
 
 var main = function () {
-        var intro = "\t\t\tMUGWUMP\n" + "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY\n\n" + "THE OBJECT OF THIS GAME IS TO FIND \n" + "FOUR MUGWUMPS HIDDEN ON A 10 BY 10 GRID.\n" + "HOMEBASE IS POSITION 0,0.\n" + "ANY GUESS YOU MAKE MUST BE TWO NUMBERS\n" + " WITH EACH NUMBER BETWEEN 0 AND 9, INCLUSIVE.\n" + "FIRST NUMBER IS DISTANCE TO RIGHT OF HOMEBASE\n" + "AND SECOND NUMBER IS DISTANCE ABOVE HOMEBASE.",
-            tries_msg = "YOU GET 10 TRIES.  AFTER EACH TRY, I WILL TELL\n" + "YOU HOW FAR YOU ARE FROM EACH MUGWUMP.",
-            mugs = prepare();
-        alert(intro);
-        alert(tries_msg);
-        game(mugs);
-        main();
-    };
-
-window.onload = main;
+    reset_game();
+    mugs = prepare();
+};
